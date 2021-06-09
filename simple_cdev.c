1a62739d43d06d693d6f8f6ef73e2636ccaa4d3a
Guillaume Thouvenin <gthouvenin@kalray.eu>
2021-06-09 15:58:57 +0200

    Enable read/write into /proc
    This allow to push data and read data from fifo using the /proc.

--- simple_cdev.c
+++ simple_cdev.c
@@ -2,94 +2,140 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/kernel.h>
+#include <linux/kfifo.h>
+#include <linux/proc_fs.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-#include <linux/kfifo.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Me");
-MODULE_DESCRIPTION("A simple char device");
+MODULE_DESCRIPTION("A simple char device backed by /proc");
 MODULE_VERSION("0.1");
 
 #define DEVICE_NAME "scdev"
-
-struct scdev_buffer_struct {
-	DECLARE_KFIFO(read_fifo, unsigned char, 128);
-	DECLARE_KFIFO(write_fifo, unsigned char, 128);
-};
+#define FIFO_SIZE 256		/* FIFO size in bytes */
 
 static int scdev_major;
 static struct class *scdev_class;
 static struct device *scdev_device;
-static struct scdev_buffer_struct scdev_buffer;
+static struct proc_dir_entry *proc_entry;
 
+enum scdev_op {
+	READ_CMDS,
+	WRITE_CMDS,
+	READ_DATA,
+	WRITE_DATA,
+};
+
+static DECLARE_KFIFO(cmds_fifo, unsigned char, FIFO_SIZE);
+static DECLARE_KFIFO(data_fifo, unsigned char, FIFO_SIZE);
+
+/*
+ * We can:
+ *
+ *  [ cmds fifo ] <- Write to /proc/scdev
+ *  [ data fifo ] <- Read from /proc/scdev
+ *
+ *  [ cmds fifo ] <- Read from /dev/scdev
+ *  [ data fifo ] <- Write from /dev/scdev
+ *
+ * It is used to simulate communication between a master and a slave where:
+ *
+ *     MASTER (/proc/scdev) |       KERNEL         | SLAVE (/dev/scdev)
+ *     ---------------------+----------------------+-------------------
+ *     write /proc/scdev    | -> [ cmds fifo ]     |
+ *                          |    [ cmds fifo ] ->  | Read the cmd
+ *                          |                      |    -> Execute the cmd
+ *                          |    [ data fifo ] <-  | Write the result
+ *    Read from /proc/scdev | <- [ data fifo ]     |
+ */
+
+static ssize_t scdev_fifo_op(enum scdev_op op, char __user *buf, size_t count)
+{
+	int ret;
+	unsigned int copied;
+
+	switch(op) {
+	case READ_CMDS:
+		ret = kfifo_to_user(&cmds_fifo, buf, count, &copied);
+		break;
+	case WRITE_CMDS:
+		ret = kfifo_from_user(&cmds_fifo, buf, count, &copied);
+		break;
+	case READ_DATA:
+		ret = kfifo_to_user(&data_fifo, buf, count, &copied);
+		break;
+	case WRITE_DATA:
+		ret = kfifo_from_user(&data_fifo, buf, count, &copied);
+		break;
+	default:
+		ret = -ERESTARTSYS;
+	}
+
+	printk(KERN_DEBUG "%s: Command %d: ret == %d, copied ==  %d", DEVICE_NAME, op, ret, copied);
+	return ret ? ret : copied;
+}
+
+/* Stuff related to /proc */
+static ssize_t scdev_proc_write(struct file *filep,
+				const char __user *buf,
+				size_t count,
+				loff_t *offset)
+{
+	return scdev_fifo_op(WRITE_CMDS, (char __user *)buf, count);
+}
+
+static ssize_t scdev_proc_read(struct file *filep,
+			       char __user *buf,
+			       size_t count,
+			       loff_t *offset)
+{
+	return scdev_fifo_op(READ_DATA, buf, count);
+}
+
+static struct file_operations scdev_proc_fops =
+{
+	.owner = THIS_MODULE,
+	.write = scdev_proc_write,
+	.read = scdev_proc_read,
+};
+
+/* Stuff related to char dev*/
 static int scdev_open(struct inode *inodep, struct file *filep)
 {
-	printk(KERN_INFO "scdev: opened\n");
+	printk(KERN_DEBUG "scdev: opened\n");
 	return 0;
 }
 
 static int scdev_release(struct inode *inodep, struct file *filep)
 {
-	printk(KERN_INFO "scdev: released\n");
+	printk(KERN_DEBUG "scdev: released\n");
 	return 0;
 }
 
-static ssize_t scdev_read(struct file *filep, char __user *buf, size_t count, loff_t *offset)
+/*  [ cmds fifo ] <- Read from char dev */
+static ssize_t scdev_read(struct file *filep,
+			  char __user *buf,
+			  size_t count,
+			  loff_t *offset)
 {
-	unsigned int copied;
-
-	printk(KERN_INFO "scdev: read called with count %lld (0x%llx) and offset %lld (0x%llx)\n",
-			(long long)count, (long long)count,
-			(long long)*offset, (long long)*offset);
-
-	if (kfifo_is_empty(&scdev_buffer.read_fifo)) {
-		printk(KERN_INFO "scdev: read buffer is empty\n");
-		return 0;
-	}
-
-	kfifo_to_user(&scdev_buffer.read_fifo, buf, count, &copied);
-	return copied;
+	return scdev_fifo_op(READ_CMDS, buf, count);
 }
 
-static ssize_t scdev_write(struct file *filep, const char __user *buf, size_t count, loff_t *offset)
+static ssize_t scdev_write(struct file *filep,
+			   const char __user *buf,
+			   size_t count,
+			   loff_t *offset)
 {
-	unsigned int copied;
-
-	printk(KERN_INFO "scdev: write called with count %lld (0x%llx) and offset %lld (0x%llx)\n",
-			(long long)count, (long long)count,
-			(long long)*offset, (long long)*offset);
-
-	if (kfifo_is_full(&scdev_buffer.write_fifo)) {
-		printk(KERN_INFO "scdev: write buffer is full\n");
-		return 0;
-	}
-
-	kfifo_from_user(&scdev_buffer.read_fifo, buf, count, &copied);
-
-	return copied;
+	return scdev_fifo_op(WRITE_DATA, (char __user *)buf, count);
 }
 
 static loff_t scdev_llseek(struct file *filep, loff_t offset, int whence)
 {
-	unsigned int val = (unsigned int) offset;
-
-	printk(KERN_INFO "scdev: llseek called with val %u and whence %d\n", val, whence);
-
-	if (kfifo_is_empty(&scdev_buffer.write_fifo)) {
-		printk(KERN_INFO "scdev: nothing has been written before this call\n");
-	} else {
-		unsigned int lu;
-		kfifo_out(&scdev_buffer.write_fifo, (unsigned char *)&lu, sizeof(lu));
-		printk(KERN_INFO "scdev: %u read from write_fifo\n", lu);
-	}
-
-	kfifo_in(&scdev_buffer.read_fifo, (const unsigned char *)&val, sizeof(val));
-
-	return 0;
+	return offset;
 }
 
-static struct file_operations fops =
+static struct file_operations scdev_fops =
 {
 	.open = scdev_open,
 	.read = scdev_read,
@@ -100,20 +146,25 @@ static struct file_operations fops =
 
 static int __init simple_cdev_init(void)
 {
-	scdev_major = register_chrdev(0, DEVICE_NAME, &fops);
+	/* Init kfifo */
+	INIT_KFIFO(cmds_fifo);
+	INIT_KFIFO(data_fifo);
+
+	/* Init char dev */
+	scdev_major = register_chrdev(0, DEVICE_NAME, &scdev_fops);
 	if (scdev_major < 0) {
 		printk(KERN_ALERT "simple_char: failed to register a major number\n");
 		return scdev_major;
 	}
-	printk(KERN_INFO "Simple char device got major %d\n", scdev_major);
+	printk(KERN_DEBUG "Simple char device got major %d\n", scdev_major);
 
-	scdev_class = class_create(THIS_MODULE, "scdev");
+	scdev_class = class_create(THIS_MODULE, DEVICE_NAME);
 	if (IS_ERR(scdev_class)) {
 		unregister_chrdev(scdev_major, DEVICE_NAME);
 		printk(KERN_ALERT "Failed to register device class\n");
 		return PTR_ERR(scdev_class);
 	}
-	printk(KERN_INFO "Simple char device class created\n");
+	printk(KERN_DEBUG "Simple char device class created\n");
 
 	scdev_device = device_create(scdev_class, NULL, MKDEV(scdev_major, 0), NULL, DEVICE_NAME);
 	if (IS_ERR(scdev_device)){
@@ -122,10 +173,11 @@ static int __init simple_cdev_init(void)
 		printk(KERN_ALERT "Simple char device: Failed to create the device\n");
 		return PTR_ERR(scdev_device);
 	}
-	printk(KERN_INFO "Simple char device has been created\n");
 
-	INIT_KFIFO(scdev_buffer.read_fifo);
-	INIT_KFIFO(scdev_buffer.write_fifo);
+	/* init /proc */
+	proc_entry = proc_create(DEVICE_NAME, 0666, NULL, &scdev_proc_fops);
+	printk(KERN_DEBUG "Simple char device has been created\n");
+
 	return 0;
 }
 
@@ -134,7 +186,8 @@ static void __exit simple_cdev_exit(void)
 	device_destroy(scdev_class, MKDEV(scdev_major, 0));
 	class_destroy(scdev_class);
 	unregister_chrdev(scdev_major, DEVICE_NAME);
-	printk(KERN_INFO "Simple char device exited\n");
+	proc_remove(proc_entry);
+	printk(KERN_DEBUG "Simple char device exited\n");
 }
 
 module_init(simple_cdev_init);
